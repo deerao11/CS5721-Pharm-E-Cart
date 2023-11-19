49019d0d89cfa968377a63393e37b6f4266e06e2
deerao11 <56555564+deerao11@users.noreply.github.com>
2023-11-19 16:22:25 +0000

    Observer pattern (#1)

---
+++ JavaApp/Control/PharmacyController.java
@@ -0,0 +1,23 @@
+package Control;
+
+import java.util.*;
+
+public class PharmacyController implements IObserver {
+
+    public void update(String orderId) {
+        manageCancellation(orderId);
+    }
+
+    public void manageCatalog() {
+
+    }
+
+    public void manageInventory() {
+
+    }
+
+    public void manageCancellation(String OrderId) {
+
+    }
+
+}
